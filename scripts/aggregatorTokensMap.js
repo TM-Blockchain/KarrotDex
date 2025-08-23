d002dfe1c6d1f3e95a862df5c351097207951780
karrotpls <karrotpls@proton.me>
2025-08-23 04:52:17 -0300

    Update and rename aggregatorTokens.PulseX to scripts/aggregatorTokensMap.js

---
+++ scripts/aggregatorTokensMap.js
@@ -0,0 +1,28 @@
+// aggregatorTokenMap.js
+export const aggregatorTokenMap = {
+  PulseX: [
+    {
+      address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+      label: "DAI",
+      logo: "img/dai.png"
+    },
+    {
+      address: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da",
+      label: "KARROT",
+      logo: "img/karrot-hex.jpg"
+    }
+  ],
+  Uniswap: [
+    {
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      label: "USDC",
+      logo: "img/usdc.png"
+    },
+    {
+      address: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da",
+      label: "KARROT",
+      logo: "img/karrot-hex.jpg"
+    }
+  ]
+  // Add more as needed
+};
