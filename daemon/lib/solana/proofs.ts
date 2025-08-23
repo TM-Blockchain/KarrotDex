aeabf6d04adc34dda85d7168e169bf50c5354446
karrotpls <karrotpls@proton.me>
2025-08-23 10:32:33 -0300

    Update proofs.ts

--- daemon/lib/solana/proofs.ts
+++ daemon/lib/solana/proofs.ts
@@ -1,1 +1,11 @@
+import { ethers } from "ethers";
+
+export function generateOrFetchProof(logs: any): string {
+  const proof = Buffer.from(JSON.stringify(logs));
+  return "0x" + proof.toString("hex");
+}
+
+export function computeProofHash(proof: string): string {
+  return ethers.utils.keccak256(proof);
+}
 
