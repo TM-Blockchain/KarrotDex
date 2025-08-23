49e4f195c8d77c98f60b903ddc7b286a36178a47
karrotpls <karrotpls@proton.me>
2025-08-23 05:57:28 -0300

    Update TokensMap.js

--- scripts/TokensMap.js
+++ scripts/TokensMap.js
@@ -1,5 +1,6 @@
 // /scripts/tokenMap.js
-export const tokenMap = {
+
+export const aggregatorTokenMap = {
   PulseX: [
     {
       address: "0x6b175474e89094c44da98b954eedeac495271d0f",
@@ -24,4 +25,16 @@ export const tokenMap = {
       logo: "img/karrot-hex.jpg"
     }
   ]
+};
+
+// Optional: default token selections
+export const aggregatorDefaults = {
+  PulseX: {
+    from: "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    to: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"   // KARROT
+  },
+  Uniswap: {
+    from: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    to: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"   // KARROT
+  }
 };
