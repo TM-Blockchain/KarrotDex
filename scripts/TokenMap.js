270103f1b6d969a2c41cdc76da00071f2ec00bdb
karrotpls <karrotpls@proton.me>
2025-08-23 13:19:10 -0300

    Create TokenMap.js

---
+++ scripts/TokenMap.js
@@ -0,0 +1,39 @@
+// /scripts/tokenMap.js
+
+export const aggregatorTokenMap = {
+  PulseX: [
+    {
+      address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+      label: "DAI",
+      logo: "img/dai.png"
+    },
+    {
+      address: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da",
+      label: "KARROT",
+      logo: "img/karrot-hex.jpg"
+    }
+  ],
+  Uniswap: [
+    {
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      label: "USDC",
+      logo: "img/usdc.png"
+    },
+    {
+      address: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da",
+      label: "KARROT",
+      logo: "img/karrot-hex.jpg"
+    }
+  ]
+};
+
+export const aggregatorDefaults = {
+  PulseX: {
+    from: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    to: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"
+  },
+  Uniswap: {
+    from: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    to: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"
+  }
+};
