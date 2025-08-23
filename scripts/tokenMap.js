25f2b363bb2efcdb4ebd17e5eb27497037fe4eff
karrotpls <karrotpls@proton.me>
2025-08-23 13:13:49 -0300

    Update tokenMap.js

--- scripts/tokenMap.js
+++ scripts/tokenMap.js
@@ -39,8 +39,21 @@ export const aggregatorDefaults = {
 };
 
 console.log(aggregatorTokenMap[state.selectedAggregator]);
-    to:   "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"
-          "0x6b175474e89094c44da98b954eedeac495271d0f",
-          "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-          "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"},
- { 
+    to:   [
+  {
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    label: "DAI",
+    logo: "img/dai.png"
+  },
+  {
+    address: "0x6910076eee8f4b6ea251b7cca1052dd744fc04da",
+    label: "KARROT",
+    logo: "img/karrot-hex.jpg"
+  },
+  {
+    address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    label: "USDC",
+    logo: "img/usdc.png"
+  }
+]
+
