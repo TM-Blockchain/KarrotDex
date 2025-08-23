dc54bc0e4b86cde08a4fefcbf2bd131e1db596df
karrotpls <karrotpls@proton.me>
2025-08-23 13:04:22 -0300

    Update tokenMap.js

--- scripts/tokenMap.js
+++ scripts/tokenMap.js
@@ -37,3 +37,10 @@ export const aggregatorDefaults = {
     to:   "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"
   }
 };
+
+console.log(aggregatorTokenMap[state.selectedAggregator]);
+    to:   "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"
+          "0x6b175474e89094c44da98b954eedeac495271d0f",
+          "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+          "0x6910076eee8f4b6ea251b7cca1052dd744fc04da"},
+ { 
